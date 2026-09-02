18a7523dad041597c1fa169e231eaa924a4bb65d
Ihsan Alif <ihsanalif10@gmail.com>
2026-09-02 18:01:38 +0700

    fix backend notes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -34,10 +34,10 @@ let notes = [
   }
 ]
 
-// app.get('/', (request, response) => {
+app.get('/', (request, response) => {
   
-//   response.send('<h1>Hello World</h1>')
-// })
+  response.send('<h1>Hello World</h1>')
+})
 
 app.get('/api/notes', (request, response) => {
   
