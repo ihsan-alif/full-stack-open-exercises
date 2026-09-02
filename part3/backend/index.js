d6a8653d20da75ee82d0ec8c832eb8eeb10e2798
Ihsan Alif <ihsanalif10@gmail.com>
2026-09-02 17:04:19 +0700

    fix backend notes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (req, res, next) => {
     console.log('Methods:   ', req.method)
@@ -95,7 +96,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
